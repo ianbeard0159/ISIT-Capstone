6f24121190cbfc8eb0145cecd13a4acbceedea8f
Hnicoulin <hmn180nlwh@gmail.com>
2023-03-16 10:49:12 -0700

    Created Enums Folder
    Enums folder created in the prefabs folder. In the Enums folder there is a C# file with public enum Trick Features.

---
+++ Assets/Prefabs/Enums/TrickFeatures.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TrickFeatures
+{
+    AngeledPipeRail,
+    SnakedRail,
+    Kicker
+};
+//The names of the trick features
+//a trick scriptable object will use this list
+//to be able to make the mover react in a certain way
+//depending on the trick
