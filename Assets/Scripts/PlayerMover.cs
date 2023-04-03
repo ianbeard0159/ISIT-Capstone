a8160f9d302a0806fc937b0d371d8473f39d9e98
MJLablanc <55462459+MJLablanc1@users.noreply.github.com>
2023-04-03 16:05:34 -0700

    basic player mover made, player rotates left and right when looking

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -6,31 +6,51 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     private CharacterController characterController;
-    private bool _inFeature;
+    private bool _inFeature; //bool for whether player is in feature or not
+    public Rigidbody rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //Get Lerp
-        Vector3 dir = Camera.main.transform.forward;
+        //take camera direction and rotation,
+        //currently not using camera rotation.
+        Vector3 cameraDir = Camera.main.transform.forward;
+        Quaternion cameraRot = Camera.main.transform.rotation;
+
+        //player rotation needs to ignore vertical rotation
+        //take camerDir as the direction to for the rotation
+        var forRotation = cameraDir;
+        //change the y axis point to zero
+        forRotation.y = 0f;
+        //renormalize the vector
+        forRotation.Normalize();
+        //make rot quaternion using the new look forRotation, using Vector3.up for the world up direction
+        var rot = Quaternion.LookRotation(forRotation, Vector3.up);
 
         if (_inFeature)
-        { 
-                
+        {
+
         }
         else
         {
-            transform.position -= dir * Time.deltaTime;
+            //Uses the rigid body to move the player
+            //rb.AddRelativeForce(cameraDir * 2);
 
-            // Rotate the forward vector towards the target direction by one step
-           // Vector3 newDirection = Vector3.RotateTowards(transform.forward, raycast.direction, 10, 0.0f);
+            //Just moved the players position in the world by where the Camera is facing,
+            //note: this is due to the forward property being Normalized, meaning it has a magnitude of 1,
+            //so that it is positioned 1 unit in front of the player.
+            transform.position += cameraDir * Time.deltaTime;
+
+            //Controls rotation, using Lerp to rotate over time, given the object to rotate (self), final rotation, and speed
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, 1 * Time.deltaTime);
         }
     }
 }
