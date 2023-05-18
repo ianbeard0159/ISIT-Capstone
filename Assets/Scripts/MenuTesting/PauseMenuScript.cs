ae4f6888d9efc81ad9595185657166b593a65c36
Kyu Kim <kyu.kim@bellevuecollege.edu>
2023-05-18 16:54:58 -0700

    adding functionality to final scene pause menu. Resume now works both ways with spacebar debug and clicking on the Resume button. PauseMenuManager renamed to PauseMenuScript and attached to a container around the pause menu. Options panel reintroduced (not functional yet). Reset button semi-functional and will teleport user to startpoint if not on a spline.

--- Assets/Scripts/MenuTesting/PauseMenuScript.cs
+++ Assets/Scripts/MenuTesting/PauseMenuScript.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
-public class PauseMenuManager : MonoBehaviour
+public class PauseMenuScript : MonoBehaviour
 {
     public GameObject pauseMenuUI;
     public Transform playerReference;
     public Vector3 offset;
+    public GameObject startPoint;
+    public PlayerMover pMover;
     private bool isGamePaused = false;
     private bool shouldBePaused = false;
+    //private float gameSpeed = 1.0f;
 
     private void Update()
     {
@@ -20,8 +23,7 @@ public class PauseMenuManager : MonoBehaviour
         }
         if (shouldBePaused && !isGamePaused)
         {
-            Pause();
-            
+            Pause();                 
         }
         else if (!shouldBePaused && isGamePaused)
         {
@@ -33,21 +35,39 @@ public class PauseMenuManager : MonoBehaviour
     //if the game is paused but should be paused
     public void Resume()
     {
-        //make the time sacle change over time
-        //pauseMenuUI.SetActive(false);
+        //make the time scale change over time
+        pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isGamePaused = false;
+
+        //added the below line, allowing pause to be done through the pause menu "Resume" button
+        shouldBePaused = false;
+
         Debug.Log("Resume");
     }
 
-    void Pause()
+    public void Pause()
     {
-        //pauseMenuUI.SetActive(true);
+        pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isGamePaused = true;
         SetMenuPosition();
         Debug.Log("Pause");
     }
+
+    //teleports player to start point
+    //currently does not function when on a spline
+    public void TpToStartPoint()
+    {        
+        pMover.transform.position = startPoint.transform.position;
+        Resume();
+    }
+
+    //possible timescale adjustment method?
+    //public void TimescaleAdjust(float x)
+    //{
+    //    Time.timeScale = x;
+    //}
     
     void SetMenuPosition()
     {
